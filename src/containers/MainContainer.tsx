9f0ad9cda7825b918f5e92cd6b4b1a045901902a
cteyr123 <cteyr123@gmail.com>
2022-07-16 13:42:52 +0200

    main container add

--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -1,4 +1,19 @@
+import { Header } from "../components/Header";
+import { Input } from "../components/Input";
+import { FiSearch } from "react-icons/Fi";
+
 const MainContainer = () => {
-  return <div className="div">hola mundo</div>;
+  return (
+    <div className="mainContainer">
+      <div className="container">
+        <Header />
+        <div className="containerInput">
+          <Input placeholder="Search" />
+          <FiSearch className="search-icon" />
+        </div>
+        <div className="product-container"></div>
+      </div>
+    </div>
+  );
 };
 export { MainContainer };
