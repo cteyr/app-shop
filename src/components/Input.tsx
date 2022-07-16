76cce73b5528a948e88bc687daf9a5b432673ff8
cteyr123 <cteyr123@gmail.com>
2022-07-16 17:26:40 +0200

    input props add

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,10 +1,18 @@
-const Input = ({ placeholder }: IProp) => {
+const Input = ({ placeholder, value, handleInputChange }: IProp) => {
   return (
-    <input type="text" className="searchInput" placeholder={placeholder} />
+    <input
+      type="text"
+      className="searchInput"
+      placeholder={placeholder}
+      value={value}
+      onChange={handleInputChange}
+    />
   );
 };
 
 type IProp = {
   placeholder: string;
+  value?: string;
+  handleInputChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 export { Input };
