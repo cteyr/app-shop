43594464c044adfafb320658908fee6441922212
cteyr123 <cteyr123@gmail.com>
2022-07-16 22:11:52 +0200

    loading add to project

--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -5,6 +5,7 @@ import { Button } from "../components/Button";
 import { FiSearch } from "react-icons/fi";
 import { useProducts } from "../hooks/useProducts";
 import { Product } from "../types/product";
+import { Ring } from "@uiball/loaders";
 
 const MainContainer = () => {
   const [InputValue, setInputValue] = useState("");
@@ -27,32 +28,37 @@ const MainContainer = () => {
 
   return (
     <div className="mainContainer">
-      <div className="container">
-        <Header />
-        <div className="containerInput">
-          <Input
-            placeholder="Search Category"
-            value={InputValue}
-            handleInputChange={handleInputonChange}
-          />
-          <FiSearch className="search-icon" />
-        </div>
-        <div className="product-container">
-          {filterCategory().map((product) => (
-            <div key={product.id} className="item-product">
-              <img src={product.image} alt="" />
-              <div className="product-info">
-                <span className="product-tittle">{product.title}</span>
-                <span className="product-category">{product.category}</span>
-                <span className="product-price">{`$${product.price} EUR`}</span>
+      {IsLoading ? (
+        <div className="container">
+          <Header />
+          <div className="containerInput">
+            <Input
+              placeholder="Search Category"
+              value={InputValue}
+              handleInputChange={handleInputonChange}
+            />
+            <FiSearch className="search-icon" />
+          </div>
+
+          <div className="product-container">
+            {filterCategory().map((product) => (
+              <div key={product.id} className="item-product">
+                <img src={product.image} alt="" />
+                <div className="product-info">
+                  <span className="product-tittle">{product.title}</span>
+                  <span className="product-category">{product.category}</span>
+                  <span className="product-price">{`$${product.price} EUR`}</span>
+                </div>
+                <div className="button-cart-buy">
+                  <Button text="Add to Cart" />
+                </div>
               </div>
-              <div className="button-cart-buy">
-                <Button text="Add to Cart" />
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <Ring size={80} speed={0.9} color="hsl(240, 5%, 38%)" />
+      )}
     </div>
   );
 };
